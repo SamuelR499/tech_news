850c10a5cdaac567bd2d1e73a15f64ac51bcb34d
Samuelr499 <samuelribeiro2@hotmail.com>
2023-02-08 16:54:23 -0300

    feat: criar fetch, regata conteudo de uma pagina em text

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,22 @@
-# Requisito 1
+import requests
+from requests.exceptions import ConnectTimeout, HTTPError, ReadTimeout
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+
+    time.sleep(1)
+    try:
+        res = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=3
+            )
+        res.raise_for_status()
+    except (ConnectTimeout, HTTPError, ReadTimeout):
+        return None
+
+    return res.text
 
 
 # Requisito 2
