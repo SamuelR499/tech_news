2cb73046c614b03cfac173ab159924070d0afb6f
Samuelr499 <samuelribeiro2@hotmail.com>
2023-02-13 11:01:20 -0300

    test para caso  de erro em reading_plan_group_news

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,9 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import ReadingPlanService
+import pytest
 
 
 def test_reading_plan_group_news():
-    pass
+    with pytest.raises(
+        ValueError, match="Valor 'available_time' deve ser maior que zero"
+    ):
+        ReadingPlanService.group_news_for_available_time(0)
