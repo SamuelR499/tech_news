f0a902e908eb84b66e43a8fb26cec32c802b3da3
Samuelr499 <samuelribeiro2@hotmail.com>
2023-02-13 12:14:07 -0300

    feat: buscador por data, data de entrada iso

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -9,19 +10,25 @@ def search_by_title(title):
             }
         }
     projection = {"title": 1, "url": 1, "_id": 0}
-    news = db.news.find(query, projection)
-
     result = []
-
-    for new in news:
+    for new in db.news.find(query, projection):
         result.append((new["title"], new["url"]))
 
     return result
 
 
-# Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    result = []
+    try:
+        br_format_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        query = {"timestamp": br_format_date}
+
+        for new in db.news.find(query):
+            result.append((new["title"], new["url"]))
+
+    except ValueError:
+        raise ValueError("Data inválida")
+    return result
 
 
 # Requisito 9
